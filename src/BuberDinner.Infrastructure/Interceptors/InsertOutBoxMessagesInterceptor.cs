e3ecc59c01a2472979ffc2975c67d87b45a105fc
p c <muchenz@poczta.onet.pl>
2023-12-30 08:55:00 +0100

    outbox

---
+++ src/BuberDinner.Infrastructure/Interceptors/InsertOutBoxMessagesInterceptor.cs
@@ -0,0 +1,62 @@
+﻿using BuberDinner.Domain.Common.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BuberDinner.Infrastructure.Interceptors;
+internal sealed class InsertOutBoxMessagesInterceptor: SaveChangesInterceptor
+{
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+    {
+
+        if (eventData.Context is not null)
+        {
+
+        }
+
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+
+    private static void InsterOutboxMessages(DbContext dbContext)
+    {
+        var outboxMessages = dbContext
+            .ChangeTracker
+            .Entries<IHasDomainEvents>()
+            .Where(a => a.Entity.DomainEvents.Any())
+            .Select(a => a.Entity)
+            .SelectMany(enity=>
+            {
+                List<IDomainEvent> domainEvents = enity.DomainEvents.ToList();
+                enity.ClearDomainEvents();
+
+                return domainEvents;
+
+            })
+            .Select(domainEvent=> new OutBoxMessage
+            {
+                Id= Guid.NewGuid(),
+                Type=domainEvent.GetType().Name,
+                Content= JsonSerializer.Serialize(domainEvent),
+                OccureredOnUtc= DateTime.UtcNow,
+
+            })
+    }
+}
+
+
+public sealed class OutBoxMessage
+{
+    public Guid Id { get; set; }
+    public string Type { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DateTime OccureredOnUtc { get; set; }
+    public DateTime? ProcessedOnUtc { get; set; } = null;
+    public string? Error { get; set; } = null;
+
+
+}
---
+++ src/BuberDinner.Infrastructure/Persistence/Configurations/OutboxMessageConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using BuberDinner.Infrastructure.Interceptors;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuberDinner.Infrastructure.Persistence.Configurations;
+internal class OutboxMessageConfiguration : IEntityTypeConfiguration<OutBoxMessage>
+{
+    public void Configure(EntityTypeBuilder<OutBoxMessage> builder)
+    {
+        builder.ToTable("OutboxMessages");
+    }
+}
